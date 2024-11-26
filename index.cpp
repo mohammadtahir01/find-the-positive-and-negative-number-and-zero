52c7daf197bc5b422cb18a11a1064a2277fe7f56
mohammadtahir01 <mdtahirhusain0121@gmail.com>
2024-11-26 10:21:51 +0530

    updation

--- index.cpp
+++ index.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
+    char color1;
+    char color2;
     
-    cout<<"Enter n number:";
-    cin>>n;
-    if(n > 0 ){
-        cout<<"positive";
-    }else if(n < 0){ 
-        cout<<"negative";
-    }else{
-        cout<<"zero";
-    }
+   cout << "Enter the first color (r, o, g, b, y, u): ";
+   cin >> color1;
+   cout << "Enter the second color (r, o, g, b, y, u): "; 
+   cin >> color2;
+   if ((color1 == 'r' && color2 == 'o') || (color1 == 'o' && color2 == 'r')) {
+       cout<<"dark"; 
+       
+   } else if ((color1 == 'g' && color2 == 'b') || (color1 == 'b' && color2 == 'g')) { 
+       cout<<"green"; 
+       
+   } else if ((color1 == 'y' && color2 == 'u') || (color1 == 'u' && color2 == 'y')) { 
+       cout<<"yellow"; 
+       
+   } else {
+       cout<<"unknown combination"; 
+       
+   }
 }
